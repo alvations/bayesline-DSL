091b588895aa2595ce0ff6ca3795f0c8ccf7e822
alvations <alvations@gmail.com>
2017-01-26 23:34:02 +0800

    predicting...

--- predict.sh
+++ predict.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
 
-for 
- python3 predict.py bayesline.5_to_5-gram.vectorizer bayesline.5_to_5-gram.classifier > 
+for i in `seq 1 6`;
+do
+  for j in `seq $i 6`;
+  do
+    python3 predict.py bayesline.$1_to_$j-gram.vectorizer bayesline.$i_to_$j-gram.classifier > outputs/$1_to_$j.out
+  done
+done
